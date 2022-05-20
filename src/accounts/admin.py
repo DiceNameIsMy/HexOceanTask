c7fa7134922f9eeafde76f856c43a9f417710589
Nurdaulet <dicenameismy@gmail.com>
2022-05-20 16:00:15 +0600

    feat: add AccountTier to user admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(DefaultUserAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("username", "password", "imgstore_tier")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = ("username", "email", "imgstore_tier", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups", "imgstore_tier")
